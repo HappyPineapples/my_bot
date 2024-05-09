e5846af8dc81dda0fe52a6c05137ccf8c6b97c7f
Hannah Hagans <hannah.hagans@gmail.com>
2024-05-09 17:18:13 -0400

    implemented ros2 control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,24 +3,44 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
+from launch_ros.actions import SetRemap
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
     package_name='my_bot'
 
+    gazebo_params_filename = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'gazebo_params.yaml'
+    )
+
+    joystick_config_filename = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'joy_config.yaml'
+    )
+
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(package_name),'launch','rsp.launch.py'
-        )]), launch_arguments={'use_sim_tim': 'true'}.items()
+        )]), launch_arguments={
+            'use_sim_tim': 'true',
+            'use_ros2_control': 'true',
+            'use_depth_camera': 'false'
+        }.items()
     )
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py')])
+            get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py'
+            )]), launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_filename}.items()
     )
 
     spawn_entity = Node(
@@ -33,18 +53,56 @@ def generate_launch_description():
         output='screen'
     )
 
-    joy = IncludeLaunchDescription(
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
+    )
+
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_broad_spawner],
+        )
+    )
+
+    joystick = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('teleop_twist_joy'),'launch', 'teleop-launch.py'
         )]), launch_arguments={
             'joy_config': 'xbox', 
+
             # 'config_filepath': './config/joy_config.yaml'
-        }.items(),
+        }.items()
+    )
+
+    remapped_joystick = GroupAction(
+        actions=[
+            SetRemap(src='/cmd_vel',dst='/diff_cont/cmd_vel_unstamped'),
+            joystick
+        ]
     )
 
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        joy
+        # joystick,
+        remapped_joystick,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
     ])
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -13,16 +13,18 @@ import xacro
 def generate_launch_description():
 
     # Check if we're told to use sim time
-    # use_sim_time = LaunchConfiguration('use_sim_time')
-    use_sim_time = True
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+    use_depth_camera = LaunchConfiguration('use_depth_camera')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_descrpition_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' use_depth_camera:=', use_depth_camera])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -35,8 +37,15 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             description='Use sim time if true'),
-
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2 control if true'),
+        DeclareLaunchArgument(
+            'use_depth_camera',
+            default_value='false',
+            description='Use depth camera if true'),
         node_robot_state_publisher
     ])
